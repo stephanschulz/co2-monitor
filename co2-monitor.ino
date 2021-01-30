a1b6bfb9461467a27e58128086674e7fce8d6ae1
stephan schulz <stephan@antimodular.com>
2021-01-30 14:52:56 -0500

    ver 2.2
    - display version number during the first 5 seconds
    - show the humidity reading as long as voltage reading < threshold voltage
    - start fading onboard NEO pixels with red when CO2 value past threshold

--- co2-monitor.ino
+++ co2-monitor.ino
@@ -5,23 +5,30 @@
 #include <SPI.h>
 #include <SD.h>
 #include "RTClib.h"
-
 RTC_PCF8523 rtc;
 
-#define USE_SCD30
+#include <Adafruit_NeoPixel.h>
+#define NEO_PIN 8
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, NEO_PIN, NEO_GRB + NEO_KHZ800);
 
-#ifdef USE_SCD30
+float verNumber = 2.2;
+int bad_co2 = 1100;
+
 Adafruit_SCD30  scd30;
-#endif
 
+//---OLED display
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+#define BUTTON_A  9
+#define BUTTON_B  6
+#define BUTTON_C  5
+
 
 #define VBATPIN A6 //A13 //14 
 
 boolean initDone;
 unsigned long initTimer;
 
-float verNumber = 1.4;
+
 
 const int ledPin =  13;      // the number of the LED pin
 
@@ -33,7 +40,7 @@ long previousMillis = 0;        // will store last time LED was updated
 // will quickly become a bigger number than can be stored in an int.
 long interval = 1000;           // interval at which to blink (milliseconds)
 
-unsigned long writeInterval = 5000; //30000;
+unsigned long writeInterval = 30000;
 unsigned long writeTimer;
 
 unsigned long checkInterval = 2000;
@@ -42,6 +49,11 @@ unsigned long checkTimer;
 float cur_co2 = 0;
 float cur_humidity = 0;
 float cur_temperature = 0;
+float cur_measuredvbat = 0;
+boolean vbat_toggleShow;
+
+unsigned long sdCard_checkTimer;
+bool sdCard_found;
 
 void setup(void) {
   Serial.begin(115200);
@@ -50,9 +62,7 @@ void setup(void) {
 
 
 
-#ifdef USE_SCD30
   Serial.println("SCD30 OLED eCO2 meter!");
-
   // Try to initialize!
   if (!scd30.begin()) {
     Serial.println("Failed to find SCD30 chip");
@@ -61,25 +71,38 @@ void setup(void) {
     }
   }
   Serial.println("SCD30 Found!");
-#endif
 
+
+  //---OLED display
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
     for (;;); // Don't proceed, loop forever
   }
 
-#ifdef USE_SCD30
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_C, INPUT_PULLUP);
+
+
+
   if (!scd30.setMeasurementInterval(2)) {
     Serial.println("Failed to set measurement interval");
     while (1) {
       delay(10);
     }
   }
-#endif
+
 
   // set the digital pin as output:
   pinMode(ledPin, OUTPUT);
+
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show(); // Initialize all pixels to 'off'
+
+
+
 
   display.display();
   delay(500); // Pause for half second
@@ -88,165 +111,174 @@ void setup(void) {
   display.setTextColor(WHITE);
   display.setRotation(0);
 
-  setup_sd();
+  sdCard_found = setup_sd();
   setup_rtc();
 
   initDone = false;
   initTimer = millis();
   checkTimer = millis();
   writeTimer = millis();
+
+  Serial.print("setup done");
 }
 
 
 void loop() {
 
-  if ( initDone == false &&  millis() - initTimer > 5000) {
-    initDone = true;
+  //------print initial info
+  if ( initDone == false) {
 
-    Serial.print("version ");
-    Serial.println(verNumber);
+    if (millis() - initTimer > 5000) {
+      initDone = true;
 
-#ifdef USE_SCD30
-    if (scd30.selfCalibrationEnabled()) {
-      Serial.print("Self calibration enabled");
-    } else {
-      Serial.print("Self calibration disabled");
-    }
-    Serial.println();
-
-    Serial.print("Measurement Interval: ");
-    Serial.print(scd30.getMeasurementInterval());
-    Serial.println(" seconds");
-
-    Serial.print("Forced Recalibration reference: ");
-    Serial.print(scd30.getForcedCalibrationReference());
-    Serial.println(" ppm");
-
-    Serial.print("Altitude offset: ");
-    Serial.print(scd30.getAltitudeOffset());
-    Serial.println(" meters");
-
-    Serial.print("Temperature offset: ");
-    Serial.print((float)scd30.getTemperatureOffset() / 100.0);
-    Serial.println(" degrees C");
-#endif
-  }
+      Serial.print("version ");
+      Serial.println(verNumber);
 
 
-
-  if (millis() - checkTimer > checkInterval) {
-    checkTimer = millis();
-
-    //        loop_rtc();
-    //    loop_sd();
-
-    display.clearDisplay();
-    //    display.setCursor(0, 0);
-
-    display.setTextSize(1);
-    //    display.setCursor(0, 0);
-    //    display.print("v:");
-    //    display.setCursor(10, 0);
-    //    display.println(verNumber, 1);
-
-    DateTime now = rtc.now();
-    display.setCursor(0, 0);
-    display.print(now.timestamp(DateTime::TIMESTAMP_TIME));
-
-    float measuredvbat = analogRead(VBATPIN);
-    measuredvbat *= 2;    // we divided by 2, so multiply back
-    measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
-    measuredvbat /= 1024; // convert to voltage
-    //    Serial.print("VBat: " );
-    //    Serial.println(measuredvbat);
-
-
-    display.setCursor(55, 0);
-    display.println(measuredvbat);
-    display.setCursor(80, 0);
-    display.print("V");
-
-
-#ifdef USE_SCD30
-
-
-    if (scd30.dataReady()) {
-      //      display.clearDisplay();
-      //      display.setCursor(0, 0);
-
-
-
-      Serial.println("Data available!");
-
-      if (!scd30.read()) {
-        Serial.println("Error reading sensor data");
-        display.println("READ ERR");
-        display.display();
-        return;
+      if (scd30.selfCalibrationEnabled()) {
+        Serial.print("Self calibration enabled");
+      } else {
+        Serial.print("Self calibration disabled");
       }
+      Serial.println();
 
-      cur_temperature = scd30.temperature;
-      cur_co2 = scd30.eCO2;
-      cur_humidity = scd30.relative_humidity;
+      Serial.print("Measurement Interval: ");
+      Serial.print(scd30.getMeasurementInterval());
+      Serial.println(" seconds");
 
-
-      Serial.print("eCO2: ");
-      Serial.print(cur_co2, 3);
+      Serial.print("Forced Recalibration reference: ");
+      Serial.print(scd30.getForcedCalibrationReference());
       Serial.println(" ppm");
-      Serial.println("");
 
-      //      display.setTextSize(1);
+      Serial.print("Altitude offset: ");
+      Serial.print(scd30.getAltitudeOffset());
+      Serial.println(" meters");
+
+      Serial.print("Temperature offset: ");
+      Serial.print((float)scd30.getTemperatureOffset() / 100.0);
+      Serial.println(" degrees C");
+
+    } else {
+      display.clearDisplay();
+      display.setTextSize(2);
+      display.setCursor(0, 0);
+      display.print("version:");
+      display.setCursor(0, 15);
+      display.println(verNumber, 1);
+      display.display();
+
+      strip.setPixelColor(0, strip.Color(0, 0, 255));
+      strip.show();
+    }
+  } else {
+
+    //---check every 5 seconds if SD card is present
+    if (millis() - sdCard_checkTimer > 5000) {
+      sdCard_checkTimer = millis();
+      sdCard_found = setup_sd();
+    }
+
+    //--check SCD30 sensor every n seconds
+    if (millis() - checkTimer > checkInterval) {
+      checkTimer = millis();
+
+      //        loop_rtc();
+      //    loop_sd();
+
+      display.clearDisplay();
+      //    display.setCursor(0, 0);
+
+      display.setTextSize(1);
+      //    display.setCursor(0, 0);
+      //    display.print("v:");
+      //    display.setCursor(10, 0);
+      //    display.println(verNumber, 1);
+
+      DateTime now = rtc.now();
+      display.setCursor(0, 0);
+      display.print(now.timestamp(DateTime::TIMESTAMP_TIME));
+
+      if (scd30.dataReady()) {
+
+        Serial.println("Data available!");
+
+        if (!scd30.read()) {
+          Serial.println("Error reading sensor data");
+          display.println("READ ERR");
+          display.display();
+          return;
+        }
+
+        cur_temperature = scd30.temperature;
+        cur_co2 = scd30.eCO2;
+        cur_humidity = scd30.relative_humidity;
 
 
-      //    display.setCursor(90, 5);
-      //    display.print("V:");
-      //    display.setCursor(100, 5);
-      //    display.println(measuredvbat);
-
-      //    display.display();
+        Serial.print("eCO2: ");
+        Serial.print(cur_co2, 3);
+        Serial.println(" ppm");
+        Serial.println("");
 
 
-      if (cur_co2 > 0 && millis() - writeTimer > writeInterval) {
-        writeTimer = millis();
+        cur_measuredvbat = analogRead(VBATPIN);
+        cur_measuredvbat *= 2;    // we divided by 2, so multiply back
+        cur_measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+        cur_measuredvbat /= 1024; // convert to voltage
 
-        logTo_sd(measuredvbat, cur_co2, cur_temperature, cur_humidity);
+        if (cur_co2 > 0 && millis() - writeTimer > writeInterval) {
+          writeTimer = millis();
+          logTo_sd(cur_measuredvbat, cur_co2, cur_temperature, cur_humidity);
+        }
+
+      } //end if (scd30.dataReady())
+
+
+      if (sdCard_found == true) {
+
+        if (cur_measuredvbat <= 3.3 ) {
+          if (vbat_toggleShow == true) {
+            display.setCursor(55, 0);
+            display.println(cur_measuredvbat);
+            display.setCursor(80, 0);
+            display.print("V");
+          }
+          vbat_toggleShow = !vbat_toggleShow;
+        } else {
+          display.setCursor(55, 0);
+          display.println(cur_humidity, 1);
+          display.setCursor(80, 0);
+          display.print("%");
+        }
+
+        display.setCursor(95, 0);
+        display.print(cur_temperature, 1);
+        display.setCursor(120, 0);
+        display.print("C");
+
+      } else {
+        display.setCursor(55, 0);
+        display.println("NO SD-CARD!");
       }
 
-    } //end if (scd30.dataReady())
-#else
+      display.setCursor(100, 10);
+      display.println(" CO2");
+      display.setCursor(100, 20);
+      display.println(" ppm");
 
+      display.setTextSize(2);
+      display.setCursor(15, 15);
+      display.print(cur_co2, 1);
+      display.display();
 
+    } //end if (millis() - writeTimer > writeInterval)
 
-#endif
+    if (cur_co2 > bad_co2) {
+      fadeNeoPixel(true);
+    } else {
+      fadeNeoPixel(false);
+    }
 
-    display.setCursor(95, 0);
-    display.print(cur_temperature, 1);
-    display.setCursor(120, 0);
-    display.print("C");
-
-    display.setCursor(100, 10);
-    display.println(" CO2");
-    display.setCursor(100, 20);
-    display.println(" ppm");
-
-    display.setTextSize(2);
-    display.setCursor(15, 15);
-    display.print(cur_co2, 1);
-
-    //#ifdef USE_SCD30
-    //    if (scd30.eCO2 > 0) {
-    //      //      logTo_sd(measuredvbat, scd30.eCO2, scd30.temperature, scd30.relative_humidity);
-    //    }
-    //#else
-    //    logTo_sd(measuredvbat, 123.45, 2, 3);
-    //#endif
-
-    display.display();
-
-  } //end if (millis() - writeTimer > writeInterval)
-
-
-
+  }//else end if initDone == false
   blink_it();
 
   //  delay(100);
@@ -275,6 +307,22 @@ void blink_it()
 
     // set the LED with the ledState of the variable:
     digitalWrite(ledPin, ledState);
+  }
+
+}
+
+void fadeNeoPixel(bool _doIt) {
+  if (_doIt) {
+    int temp_percent = map(millis() % 10000, 0, 10000, -255, 255);
+    //    Serial.print("temp_percent ");
+    //    Serial.print(temp_percent);
+    //    Serial.println();
+
+    strip.setPixelColor(0, strip.Color(abs(temp_percent), 0, 0));
+    strip.show();
+  } else {
+    strip.setPixelColor(0, strip.Color(0, 0, 0));
+    strip.show();
   }
 
 }
--- sd_card.ino
+++ sd_card.ino
@@ -2,76 +2,22 @@
 #define cardSelect 10 // 15 for huzzah
 File logfile;
 
-void setup_sd() {
+bool setup_sd() {
 
 
   // see if the card is present and can be initialized:
   if (!SD.begin(cardSelect)) {
     Serial.println("Card init. failed!");
-    error(2);
+    return false;
   }
 
 
   pinMode(13, OUTPUT);
   pinMode(8, OUTPUT);
   Serial.println("Ready!");
+  return true;
 }
 
-/*
-  void loop_sd() {
-
-  DateTime now = rtc.now();
-
-  String dataString = "";
-
-  dataString += String(now.year(), DEC);
-  dataString += '/';
-  dataString += String(now.month(), DEC);
-  dataString += '/';
-  dataString += String(now.day(), DEC);
-  dataString += " (";
-  dataString += daysOfTheWeek[now.dayOfTheWeek()];
-  dataString += ") ";
-  dataString += String(now.hour(), DEC);
-  dataString += ':';
-  dataString += String(now.minute(), DEC);
-  dataString += ':';
-  dataString += String(now.second(), DEC);
-  //  dataString += '\n';
-  Serial.println(dataString);
-
-  digitalWrite(8, HIGH);
-
-  if (logfile) {
-    logfile.println(dataString);
-    logfile.println(String("DateTime::TIMESTAMP_FULL:\t") + now.timestamp(DateTime::TIMESTAMP_FULL));
-    logfile.print("A0 = "); logfile.println(analogRead(0));
-
-    Serial.println(String("DateTime::TIMESTAMP_FULL:\t") + now.timestamp(DateTime::TIMESTAMP_FULL));
-    Serial.print("A0 = "); Serial.println(analogRead(0));
-  }
-  digitalWrite(8, LOW);
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
-  // if the file is available, write to it:
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-    // print to the serial port too:
-    Serial.println(dataString);
-  }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening datalog.txt");
-  }
-
-  delay(100);
-
-  }
-*/
 
 void logTo_sd(float batV_value, float co2_value, float temp, float humidity) {
 
@@ -148,20 +94,4 @@ void logTo_sd(float batV_value, float co2_value, float temp, float humidity) {
   }
 
   delay(100);
-}
-
-// blink out an error code
-void error(uint8_t errno) {
-  while (1) {
-    uint8_t i;
-    for (i = 0; i < errno; i++) {
-      digitalWrite(13, HIGH);
-      delay(100);
-      digitalWrite(13, LOW);
-      delay(100);
-    }
-    for (i = errno; i < 10; i++) {
-      delay(200);
-    }
-  }
 }
